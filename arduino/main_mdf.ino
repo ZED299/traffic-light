2f269b93ea38a81866545469d316b2c6166352b0
Nguyễn Đức Thuận <93931420+ZED299@users.noreply.github.com>
2025-03-09 15:23:43 +0700

    main_mdf.ino

--- arduino/main_mdf.ino
+++ arduino/main_mdf.ino
@@ -11,29 +11,27 @@
 *******************************************************************************/
 
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(5);
+  Serial.begin(Baud_rate);
+  Serial.setTimeout(Time_out);
   pinmode();
 
   DS1307.begin();
-//  DS1307.stop();
-//  uint16_t setTimeBuff[7] = {5, 52, 15, 5, 23, 5, 2024};// giây, phút , giờ, thứ, ngày , tháng , năm
-//  DS1307.setTime(setTimeBuff);
-//  DS1307.saveTimeToEEPROM();
-//  DS1307.setTimeFromEEPROM();
-   DS1307.start();
+  DS1307.stop();
+  /*uint16_t setTimeBuff[7] = {00, 57, 22, 5, 27, 2, 2025};// giây, phút , giờ, thứ, ngày , tháng , năm
+  DS1307.setTime(setTimeBuff);
+  DS1307.saveTimeToEEPROM();
+  DS1307.setTimeFromEEPROM();
+  DS1307.start();*/
   DS1307.setSqwPinMode(DS1307.eSquareWave_1Hz);
 
   blink();
 }
 
 void loop() {
-  rst_time_ndb();
   read_btn();
   runled();
   display();
   get_time();
-  //check_btn();
 }
 
 /*******************************************************************************
